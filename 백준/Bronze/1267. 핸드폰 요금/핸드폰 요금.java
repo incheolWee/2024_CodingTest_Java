e64518f948025f73a20874067273b9e9f8126252
위인철 <126574194+incheolWee@users.noreply.github.com>
2024-01-15 11:31:33 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 240 ms, Memory: 18252 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+
+            int count = s.nextInt();
+            int num[] = new int[count];
+            int M = 0; // 민식 60초마다 15
+            int Y = 0; // 영식 30초마다 10
+            for (int i = 0; i < count; i++) {
+                num[i] = s.nextInt();
+                M += ((num[i] / 60) + 1) * 15;
+                Y += ((num[i] / 30) + 1) * 10;
+            }
+
+            if (M == Y) {
+                System.out.println("Y M " + M);
+            } else if (M > Y) {
+                System.out.println("Y " + Y);
+            } else if (M < Y) {
+                System.out.println("M " + M);
+            }
+
+        }
+
+    }
+}
