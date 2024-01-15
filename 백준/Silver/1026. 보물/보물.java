7c00f8672c010c7f30ac7f7cffef34bd91cc4642
위인철 <126574194+incheolWee@users.noreply.github.com>
2024-01-15 10:11:37 +0900

    [Silver IV] Title: 보물, Time: 212 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int input = s.nextInt();
+
+        int num_A[] = new int[input];
+        int num_B[] = new int[input];
+
+        // A 입력
+        for (int i = 0; i < input; i++) {
+            num_A[i] = s.nextInt();
+        }
+        // B 입력
+        for (int i = 0; i < input; i++) {
+            num_B[i] = s.nextInt();
+        }
+        Arrays.sort(num_A);
+        Arrays.sort(num_B);
+        int sum = 0;
+        for (int i = 0; i < input; i++) {
+            sum += num_A[i] * num_B[input - i - 1];
+        }
+        System.out.println(sum);
+        s.close();
+    }
+}
