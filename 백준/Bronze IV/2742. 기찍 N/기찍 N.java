9a1cad00b7eef959ee0d7eeac50e0471cf20de39
위인철 <126574194+incheolWee@users.noreply.github.com>
2024-02-14 23:16:31 +0900

    [Bronze IV] Title: 기찍 N, Time: 1112 ms, Memory: 31508 KB -BaekjoonHub

---
+++ 백준/Bronze IV/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
